63959e030b840a9672f4ece29d51402a60e1843c
Senuoy-16 <youn6197@gmail.com>
2025-11-25 12:41:57 +0100

    modify the code and add new section top-cars

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,17 +1,21 @@
-    const hamburger = document.querySelector('.hamburger');
-    const mobileMenu = document.querySelector('.mobile-menu');
+///////////////////////////NAVBAR///////////////////////////
+const hamburger   = document.querySelector('.hamburger');
+const mobileMenu  = document.querySelector('.mobile-menu');
+const navbar      = document.querySelector('nav.navbar');
 
-    const toggleMenu = () => {
-        hamburger.classList.toggle('hamburger-active');
-        mobileMenu.classList.toggle('active');
-    };
+const toggleMenu = () => {
+    hamburger.classList.toggle('hamburger-active');
+    mobileMenu.classList.toggle('active');
+    navbar.classList.toggle('active');
+};
 
-    hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
-    const mobileLinks = document.querySelectorAll('.mobile-menu-content a');
-    mobileLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            hamburger.classList.remove('hamburger-active');
-            mobileMenu.classList.remove('active');
-        });
+const mobileLinks = document.querySelectorAll('.mobile-menu-content a');
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('hamburger-active');
+        mobileMenu.classList.remove('active');
     });
+});
+///////////////////////////NAVBAR///////////////////////////
