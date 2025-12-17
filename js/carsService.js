92fe2613dc4bcee65c200478f922f1018c458317
marwane <marwanefakir1949@gmail.com>
2025-12-17 14:34:38 +0100

    Add carsService

---
+++ js/carsService.js
@@ -0,0 +1,35 @@
+const loadCars = async () => {
+    try {
+        const res = await fetch('../cars.json');
+        if (!res.ok) throw new Error('Failed to load cars');
+        return await res.json();
+    } catch (e) {
+        console.error(e);
+        return [];
+    }
+};
+
+export const getAllCars = async () => {
+    return await loadCars();
+};
+
+export const getCarById = async (id) => {
+    const cars = await loadCars();
+    return cars.find(car => car.id === id);
+};
+
+export const getCarsByName = async (name) => {
+    const cars = await loadCars();
+    return cars.filter(car => car.modelName.toLowerCase().includes(name.toLowerCase()));
+};
+
+export const getCarsByBrand = async (brand) => {
+    const cars = await loadCars();
+    return cars.filter(car => car.brand.toLowerCase() === brand.toLowerCase());
+};
+
+export const getPopularCars = async () => {
+    const cars = await loadCars();
+    return [cars[0], cars[1], cars[2]];
+}
+
