3f2355feb0fa88cc9d2d094018df5201d011d3c5
Younes B'zize <younesbzize0@gmail.com>
2025-12-17 18:27:08 +0100

    added a n image slider (js)

--- js/showcase-base-design.js
+++ js/showcase-base-design.js
@@ -1,3 +1,7 @@
+
+/////////////Calculate total price code \\\\\\\\\\\\\\\
+
+
 const daysInput = document.getElementById('days');
 const total = document.getElementById('totalDisplay');
 
@@ -9,3 +13,53 @@ daysInput.addEventListener('input', function() {
     total.innerText = total1 + 'DH';
 });
 
+
+// ///////Scroll images code \\\\\\\\\\\\\\\
+
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const image = document.getElementsByTagName('img')[0];
+
+
+const img_lst = ["assets/images/cars/Porshe-911-GT3-RS/img_1.webp",
+                "assets/images/cars/Porshe-911-GT3-RS/img_2.webp",
+                "assets/images/cars/Porshe-911-GT3-RS/img_3.webp",
+                "assets/images/cars/Porshe-911-GT3-RS/img_4.webp",
+                "assets/images/cars/Porshe-911-GT3-RS/img_5.webp",
+                "assets/images/cars/Porshe-911-GT3-RS/img_6.webp",
+                "assets/images/cars/Porshe-911-GT3-RS/img_7.webp"
+               ];
+
+
+// slide to right
+
+rightBtn.addEventListener('click', function() {
+
+    let cheminRelatif = image.getAttribute('src');
+    let position = img_lst.indexOf(cheminRelatif);
+    
+
+        if (position == 6) {
+            position = -1;
+        }
+
+        image.src = img_lst[position + 1];
+
+});
+
+
+// slide to left
+
+leftBtn.addEventListener('click', function() {
+
+    let cheminRelatif = image.getAttribute('src');
+    let position = img_lst.indexOf(cheminRelatif);
+    
+
+        if (position == 0) {
+            position = 7;
+        }
+
+        image.src = img_lst[position - 1];
+
+});
